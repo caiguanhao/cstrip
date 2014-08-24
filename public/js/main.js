b75a8a134ffa4a4bbc0807e42f21e42885724e0d
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-08-24 20:13:32 +0800

    allow user to edit and save content;

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ config(['$routeProvider', '$locationProvider',
     controller: 'MainController as main',
     resolve: {
       cs: ['CommitStrip', function(CommitStrip) {
-        return CommitStrip.then(function(res) {
+        return CommitStrip.get().then(function(res) {
           return res.data;
         });
       }]
@@ -38,12 +38,22 @@ directive('showWhenLoadCompletes', [function() {
   };
 }]).
 
-factory('CommitStrip', ['$http', function($http) {
-  return $http.get('/commitstrip.json');
+service('CommitStrip', ['$http', function($http) {
+  this.get = function() {
+    return $http.get('/commitstrip.json');
+  };
+  this.update = function(index, content) {
+    var data = { content: content };
+    return $http.post('/update/' + index, data);
+  };
 }]).
 
-controller('MainController', ['$routeParams', 'cs',
-  function($routeParams, cs) {
+controller('MainController', ['$routeParams', 'cs', 'CommitStrip',
+  function($routeParams, cs, CommitStrip) {
+  var that = this;
   this.cs = cs;
   this.index = +$routeParams.CSID || 0;
+  this.save = function() {
+    CommitStrip.update(that.index, that.cs[that.index].content);
+  };
 }]);
--- server.go
+++ server.go
@@ -1,11 +1,61 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
+
+const (
+	DATA_FILE = "data/commitstrip.json"
+)
+
+type Document struct {
+	URL     string    `json:"url"`
+	Title   string    `json:"title"`
+	Date    time.Time `json:"date"`
+	Image   string    `json:"image"`
+	Content string    `json:"content"`
+}
+
+type CommitStrip struct {
+	Content string `json:"content" binding:"required"`
+}
+
+func getDocuments() []Document {
+	data, _ := ioutil.ReadFile(DATA_FILE)
+	var documents []Document
+	json.Unmarshal(data, &documents)
+	return documents
+}
+
+func saveDocuments(documents []Document) {
+	jsonFile, _ := json.MarshalIndent(documents, "", "  ")
+	ioutil.WriteFile(DATA_FILE, jsonFile, 0644)
+}
 
 func main() {
 	m := martini.Classic()
 	m.Use(martini.Static("data"))
+	m.Post("/update/:index", binding.Bind(CommitStrip{}),
+		func(params martini.Params, cs CommitStrip, res http.ResponseWriter) {
+			var index int = -1
+			fmt.Sscanf(params["index"], "%d", &index)
+			if index < 0 {
+				res.WriteHeader(http.StatusNotFound)
+				return
+			}
+			documents := getDocuments()
+			if index > len(documents)-1 {
+				res.WriteHeader(http.StatusNotFound)
+				return
+			}
+			documents[index].Content = cs.Content
+			saveDocuments(documents)
+		})
 	m.Run()
 }
